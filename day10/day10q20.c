dc2a5a6887114c4067ea9ceb79028c52cc270762
pratha08 <ganeshiapratha08@gmail.com>
2026-08-25 12:00:00 +0000

    100 Days of Code: Day 10

---
+++ day10/day10q20.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+    // Declare variable for day number
+    int day;
+
+    // Prompt user for input
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    // Use switch-case to determine the day
+    switch (day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid input\n");
+            break;
+    }
+
+    return 0;
+}
