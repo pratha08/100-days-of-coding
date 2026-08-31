e9420c0ceae05d5ebd44aac28c2bd81e5d7c883a
pratha08 <ganeshiapratha08@gmail.com>
2026-08-31 12:00:00 +0000

    100 Days of Code: Day 16

---
+++ day16/program13.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j;
+
+    printf("Enter the elements of a given matrix:\n");
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            b[i][j]=a[j][i];
+        }
+    }
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%2d ",b[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
